f390ce732259b6fb4b0b4a62e2f655822539ecd6
WywySenarios <wywyericzhu@gmail.com>
2026-02-12 20:56:17 -0500

    Update ssh_backup_public_key location

--- apps/postgres/entrypoint.sh
+++ apps/postgres/entrypoint.sh
@@ -6,10 +6,10 @@ chmod 400 "/tmp/Wywy-Website/id_ed25519"
 chown postgres:postgres "/tmp/Wywy-Website/id_ed25519"
 
 mkdir -p /tmp/Wywy-Website/backup
-cp run/secrets/ssh_backup_private_key /tmp/Wywy-Website/backup/id_ed25519.pub
-chmod 400 tmp/Wywy-Website/backup/id_ed25519.pub
-chown postgres:postgres /tmp/Wywy-Website/backup/id_ed25519.pub
+cp run/secrets/ssh_backup_private_key /tmp/Wywy-Website/backup/ssh_host_ed25519_key.pub
+chmod 400 tmp/Wywy-Website/backup/ssh_host_ed25519_key.pub
+chown postgres:postgres /tmp/Wywy-Website/backup/ssh_host_ed25519_key.pub
 
-echo "$BACKUP_HOST $(cat /tmp/Wywy-Website/backup/id_ed25519.pub)" > /root/.ssh/known_hosts
+echo "$BACKUP_HOST $(cat /tmp/Wywy-Website/backup/ssh_host_ed25519_key.pub)" > /root/.ssh/known_hosts
 
 exec su postgres -c "postgres -D /var/lib/postgresql/data -p $DATABASE_PORT -c config_file=/etc/postgresql/postgresql.conf"
