1bd4e4ac9a328f1f5ee77de69b03be42cc6fe36a
WywySenarios <wywyericzhu@gmail.com>
2026-03-12 10:52:52 -0400

    Add table purge helper

---
+++ apps/tests/database/purge.py
@@ -0,0 +1,19 @@
+import psycopg
+from psycopg import sql
+from .transform import table_transform, TransformTargets
+
+
+def purge_transformation(cur: psycopg.Cursor, targets: TransformTargets):
+    cur.execute(
+        sql.SQL(
+            """
+                    TRUNCATE TABLE {table_names} CASCADE RESTART IDENTITY;
+                    """
+        ).format(table_names=sql.SQL(", ").join(targets.keys()))
+    )
+
+
+def purge_database():
+    """Purges the Postgres database."""
+
+    table_transform(purge_transformation)
