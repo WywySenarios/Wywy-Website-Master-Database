6376c9d6811e99811d039623e0dd29bf6bea3793
WywySenarios <wywyericzhu@gmail.com>
2026-03-13 22:47:36 -0400

    Fix test docker container
    * Downgraded from alpha build (alpha is unstable, psycopg-binary version is not ready)
    * Added docker volumes

--- apps/tests/config.py
+++ apps/tests/config.py
@@ -2,5 +2,5 @@ import yaml
 from .Wywy_Website_Types import MainConfig
 
 # peak at config
-with open("config.yml", "r") as file:
+with open("/config.yml", "r") as file:
     CONFIG: MainConfig = yaml.safe_load(file)
