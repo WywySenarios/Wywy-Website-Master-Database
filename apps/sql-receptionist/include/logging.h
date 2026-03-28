ed7bd4e412ad155146d783192851354eb219712d
WywySenarios <wywyericzhu@gmail.com>
2026-03-28 09:40:08 -0400

    Fix logging initialization

--- apps/sql-receptionist/include/logging.h
+++ apps/sql-receptionist/include/logging.h
@@ -1,69 +1,16 @@
 #include <stdio.h>
 
-#ifndef LOGGER_INITIALIZED
-#define LOGGER_INITIALIZED
-
-void initialize_logger();
-
-#endif
-
 extern FILE *logger;
 extern FILE *debug_logger;
 
-#define log_critical(text)                                                     \
-  do {                                                                         \
-    fputs(text, stderr);                                                       \
-    fputs(text, logger);                                                       \
-    fputs(text, debug_logger);                                                 \
-  } while (0)
-#define log_error(text)                                                        \
-  do {                                                                         \
-    fputs(text, stderr);                                                       \
-    fputs(text, logger);                                                       \
-    fputs(text, debug_logger);                                                 \
-  } while (0)
-#define log_warn(text)                                                         \
-  do {                                                                         \
-    fputs(text, stderr);                                                       \
-    fputs(text, logger);                                                       \
-    fputs(text, debug_logger);                                                 \
-  } while (0)
-#define log_info(text)                                                         \
-  do {                                                                         \
-    puts(text);                                                                \
-    fputs(text, logger);                                                       \
-    fputs(text, debug_logger);                                                 \
-  } while (0)
-#define log_debug(text)                                                        \
-  do {                                                                         \
-    fputs(text, debug_logger);                                                 \
-  } while (0)
+extern void log_critical(const char *message);
+extern void log_error(const char *message);
+extern void log_warn(const char *message);
+extern void log_info(const char *message);
+extern void log_debug(const char *message);
 
-#define log_critical_printf(format, ...)                                       \
-  do {                                                                         \
-    fprintf(stderr, format, __VA_ARGS__);                                      \
-    fprintf(logger, format, __VA_ARGS__);                                      \
-    fprintf(debug_logger, format, __VA_ARGS__);                                \
-  } while (0)
-#define log_error_printf(format, ...)                                          \
-  do {                                                                         \
-    fprintf(stderr, format, __VA_ARGS__);                                      \
-    fprintf(logger, format, __VA_ARGS__);                                      \
-    fprintf(debug_logger, format, __VA_ARGS__);                                \
-  } while (0)
-#define log_warn_printf(format, ...)                                           \
-  do {                                                                         \
-    fprintf(stderr, format, __VA_ARGS__);                                      \
-    fprintf(logger, format, __VA_ARGS__);                                      \
-    fprintf(debug_logger, format, __VA_ARGS__);                                \
-  } while (0)
-#define log_info_printf(format, ...)                                           \
-  do {                                                                         \
-    printf(format, __VA_ARGS__);                                               \
-    fprintf(logger, format, __VA_ARGS__);                                      \
-    fprintf(debug_logger, format, __VA_ARGS__);                                \
-  } while (0)
-#define log_debug_printf(format, ...)                                          \
-  do {                                                                         \
-    fprintf(debug_logger, format, __VA_ARGS__);                                \
-  } while (0)
+extern void log_critical_printf(const char *format, ...);
+extern void log_error_printf(const char *format, ...);
+extern void log_warn_printf(const char *format, ...);
+extern void log_info_printf(const char *format, ...);
+extern void log_debug_printf(const char *format, ...);
