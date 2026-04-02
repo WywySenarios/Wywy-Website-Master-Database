fe7fcbbe0b6175468950b5ad727aab9851a0df91
WywySenarios <wywyericzhu@gmail.com>
2026-04-01 22:47:36 -0400

    Better fail messages

--- apps/tests/endpoint_security_tests.py
+++ apps/tests/endpoint_security_tests.py
@@ -9,13 +9,15 @@ def test_endpoint_security(test_object: unittest.TestCase, endpoint: str) -> Non
     test_object.assertEqual(requests.get(endpoint).status_code, 400)
 
     # is the endpoint secured with authentication?
+    response = requests.post(
+        endpoint,
+        headers={"Origin": environ["MAIN_URL"]},
+        data="",
+    )
     test_object.assertEqual(
-        requests.post(
-            endpoint,
-            headers={"Origin": environ["MAIN_URL"]},
-            data="",
-        ).status_code,
+        response.status_code,
         401,
+        f"Unauthenticated response did not respond with status 401: {response.status_code}: {response.text}",
     )
 
     # check if modifying authentication details invalidates the authentication
@@ -23,26 +25,30 @@ def test_endpoint_security(test_object: unittest.TestCase, endpoint: str) -> Non
         cookies = {**AUTH_COOKIES}
         del cookies[key]
 
+        response = requests.post(
+            endpoint,
+            cookies=cookies,
+            headers={"Origin": environ["MAIN_URL"]},
+            data="",
+        )
         test_object.assertEqual(
-            requests.post(
-                endpoint,
-                cookies=cookies,
-                headers={"Origin": environ["MAIN_URL"]},
-                data="",
-            ).status_code,
+            response.status_code,
             403,
+            f"Invalid authentication (missing authentication attribute) did not respond with status 403: {response.status_code}: {response.text}",
         )
 
     for key in AUTH_COOKIES:
         cookies = AUTH_COOKIES
         cookies[key] += " <- is now invalid"
 
+        response = requests.post(
+            endpoint,
+            cookies=cookies,
+            headers={"Origin": environ["MAIN_URL"]},
+            data="",
+        )
         test_object.assertEqual(
-            requests.post(
-                endpoint,
-                cookies=cookies,
-                headers={"Origin": environ["MAIN_URL"]},
-                data="",
-            ).status_code,
+            response.status_code,
             403,
+            f"Invalid authentication (incorrect attribute) did not respond with status 403: {response.status_code}: {response.text}",
         )
