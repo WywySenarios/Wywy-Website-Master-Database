aa276253e090d4314e4ed2a0bf9effda4835c054
WywySenarios <wywyericzhu@gmail.com>
2026-05-07 18:59:45 -0400

    Run login tests

--- apps/unit_tests/src/main.c
+++ apps/unit_tests/src/main.c
@@ -1,4 +1,5 @@
 #include "auth/test_creds.h"
+#include "auth/test_login.h"
 #include "auth/test_rng.h"
 #include "auth/test_session.h"
 #include "postgres/test_datatype_validation.h"
@@ -9,6 +10,9 @@ int main() {
   test_generate_secure_random_string();
   test_session();
   test_creds();
+
+  RUN_TEST_SUITE(login)
+  RUN_TEST_SUITE(handle_login)
 
   // test_check_st_point();
 
